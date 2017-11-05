8e18acad2f6557c71c7404143ad3f8205cc9de35
ryans <ryanszcz@buffalo.edu>
2017-11-05 00:40:13 -0400

    forward progress

--- app/src/main/java/com/beerme/beer_me/drinking_class.java
+++ app/src/main/java/com/beerme/beer_me/drinking_class.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by ryans on 11/4/2017.
  */
@@ -16,10 +20,13 @@ public class drinking_class extends AppCompatActivity {
     TextView _textView;
     int _drinks;
     TextView _timer;
-    double _startTime;
+    int _sec;
     double _currentBAC;
-    boolean _isMale;
-    double _weight;
+    boolean _isMale=true;
+    double _weight=200;
+    int _min;
+    boolean _started = false;
+    int _hours;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,23 +37,99 @@ public class drinking_class extends AppCompatActivity {
          _textView = (TextView) findViewById(R.id.textView);
          _timer = (TextView) findViewById(R.id.textView2);
 
+
+
+
+
+
+
+
     }
 
     public void beerMe (View v){
-        _drinks +=1;
-        _textView.setText((_drinks)+"");
+        if(_started) {
+            _drinks += 1;
+            _textView.setText((_drinks) + "");
+            TextView bac = (TextView) (findViewById(R.id.BAC));
+            currentBAC();
+            double x = Math.floor(_currentBAC*1000)/1000;
+            bac.setText(x + "");
+        }
 
 
     }
     public void crap(View v){
-        if(_drinks>0) {
-            _drinks -= 1;
-            _textView.setText((_drinks) + "");
+        if(_started) {
+            if (_drinks > 0) {
+                _drinks -= 1;
+                _textView.setText((_drinks) + "");
+                TextView bac = (TextView) (findViewById(R.id.BAC));
+                currentBAC();
+                double x = Math.floor(_currentBAC*1000)/1000;
+                bac.setText(x + "");
+            }
         }
     }
     public void letsStart(View v){
-        //FIXME
-      //  _startTime
+        _started = true;
+
+        Timer t = new Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        if(_sec == 60){
+                            _sec=0;
+                            _min=_min+1;
+
+                        }
+                        if (_min == 60){
+                            _min=0;
+                            _hours = _hours +1;
+
+                        }
+
+                        if (_hours > 0) {
+                            if(_min<10) {
+                                if (_sec < 10) {
+                                    _timer.setText(String.valueOf(_hours) + ":0" + String.valueOf(_min) + ":0" + String.valueOf(_sec));
+                                } else {
+                                    _timer.setText(String.valueOf(_hours) + ":0" + String.valueOf(_min) + ":" + String.valueOf(_sec));
+                                }
+                            }
+                            else{
+                                    if (_sec < 10) {
+                                        _timer.setText(String.valueOf(_hours) + ":" + String.valueOf(_min) + ":0" + String.valueOf(_sec));
+                                    } else {
+                                        _timer.setText(String.valueOf(_hours) + ":" + String.valueOf(_min) + ":" + String.valueOf(_sec));
+                                    }
+                                }
+
+
+                            _sec += 1;
+                        }
+                        else{
+                            if (_sec < 10) {
+                                _timer.setText(String.valueOf(_min) + ":0" + String.valueOf(_sec));
+                            } else {
+                                _timer.setText(String.valueOf(_min) + ":" + String.valueOf(_sec));
+                            }
+                            _sec += 1;
+
+                        }
+
+
+                    }
+                });
+            }
+        }, 0, 1000);
+        View start = (Button)findViewById(R.id.button2);
+        start.setVisibility(View.GONE);
+        //  _startTime
     }
 
     public void currentBAC(){
@@ -59,14 +142,15 @@ public class drinking_class extends AppCompatActivity {
         int alcInGrams = _drinks * 14;
         double bodyWeight = _weight / 0.0022046;
         double bacPercent = (alcInGrams / (bodyWeight * constant)) * 100;
-        double accurateBAC = bacPercent - (hoursSinceFirstDrink());
+        double accurateBAC = bacPercent - (hoursSinceFirstDrink()*.015);
         _currentBAC = accurateBAC;
+
     }
 
 
 
     private double hoursSinceFirstDrink(){
-        //FIXME
-        return 0;
+        double x = _min/60.0 + _hours;
+        return x;
     }
 }
